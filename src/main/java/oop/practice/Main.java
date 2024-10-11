c9a43b8fed4c9c4ec323629142cf4dee747a8da8
Moisenco Ciprian <CIPRIANMOISENCO.IT@GMAIL.COM>
2024-10-11 14:47:37 +0300

    Created read class and parsed the file as a Json Object

--- src/main/java/oop/practice/Main.java
+++ src/main/java/oop/practice/Main.java
@@ -12,17 +12,18 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) throws IOException {
 
+//    Creature creature1 = new Creature("Zaphod Beeblebrox", "Betelgeusian", "Hitchhikers");
+//    Creature creature2 = new Creature("Luke Skywalker", "Human", "Star Wars");
+//    Creature creature3 = new Creature("Frodo Baggins", "Hobbit", "Lord of the Rings");
+//
+//    creature1.printCreatureInfo();
+//    creature2.printCreatureInfo();
+//    creature3.printCreatureInfo();
+//
+//    creature1.setUniverse("Hitchhikers");
+//    creature1.printCreatureInfo();
 
-    Creature creature1 = new Creature("Zaphod Beeblebrox", "Betelgeusian", "Hitchhikers");
-    Creature creature2 = new Creature("Luke Skywalker", "Human", "Star Wars");
-    Creature creature3 = new Creature("Frodo Baggins", "Hobbit", "Lord of the Rings");
-
-    creature1.printCreatureInfo();
-    creature2.printCreatureInfo();
-    creature3.printCreatureInfo();
-
-    creature1.setUniverse("Hitchhikers");
-    creature1.printCreatureInfo();
+    Read.main(null);
 
     ObjectMapper mapper = new ObjectMapper();
     File inputFile = new File("src/main/resources/test-input.json");
@@ -33,11 +34,13 @@ public class Main {
     Universe marvel = new Universe("marvel", new ArrayList<>());
     Universe rings = new Universe("rings", new ArrayList<>());
 
+
+    System.out.println("Classify the creatures manually to their Universe");
     Scanner scanner = new Scanner(System.in);
 
     for (JsonNode entry : data) {
-      //String entryAsString = entry.toString();
-      //System.out.println(entryAsString);
+      String entryAsString = entry.toString();
+      System.out.println(entryAsString);
       String userInput = scanner.nextLine();
       switch (userInput) {
         case "1":
@@ -58,6 +61,12 @@ public class Main {
     }
 
     scanner.close();
+
+    File outputDir = new File("src/main/resources/output");
+    if (!outputDir.exists()) {
+      outputDir.mkdirs();  // create the directory if it does not exist
+    }
+
     mapper.writeValue(new File("src/main/resources/output/starwars.json"), starWars);
     mapper.writeValue(new File("src/main/resources/output/hitchhiker.json"), hitchhikers);
     mapper.writeValue(new File("src/main/resources/output/rings.json"), rings);
