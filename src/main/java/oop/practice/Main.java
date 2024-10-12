eb59b03272f080b58b0f450017d3d94636fb12c9
Moisenco Ciprian <CIPRIANMOISENCO.IT@GMAIL.COM>
2024-10-12 14:36:24 +0300

    Creatures are read from the file, classified based on their attributes, and added to the appropriate universes.

--- src/main/java/oop/practice/Main.java
+++ src/main/java/oop/practice/Main.java
@@ -15,10 +15,13 @@ public class Main {
     File inputFile = new File("src/main/resources/test-input.json");
     JsonNode data = mapper.readTree(inputFile).get("data");
 
-    System.out.println("Original JSON data from file:");
-    System.out.println(data.toPrettyString());
+    // Initialize universes for creature classification
+    Universe starWars = new Universe("StarWars", new ArrayList<>());
+    Universe hitchhikers = new Universe("Hitchhikers", new ArrayList<>());
+    Universe marvel = new Universe("Marvel", new ArrayList<>());
+    Universe rings = new Universe("Lord of the Rings", new ArrayList<>());
 
-    // list to store Creature objects - container for internal class
+    // List to store creatures
     List<Creature> creatures = new ArrayList<>();
 
     for (JsonNode entry : data) {
@@ -35,24 +38,83 @@ public class Main {
         }
       }
 
-      //  Creature instance and add to list - map JSON data to class
       Creature creature = new Creature(name, species, universe, age, isHumanoid, traits);
       creatures.add(creature);
+
+      String classification = classifyCreature(creature, starWars, hitchhikers, marvel, rings, mapper);
+      System.out.println("Creature with id:" + creatures.size() + " belongs to universe: " + classification);
+    }
+  }
+
+  private static String classifyCreature(Creature creature, Universe starWars, Universe hitchhikers, Universe marvel, Universe rings, ObjectMapper mapper) {
+    String classification = null;
+
+    // Star Wars classification
+    if (!creature.isHumanoid() && "Kashyyyk".equalsIgnoreCase(creature.getUniverse())) {
+      starWars.individuals().add(mapper.valueToTree(creature));
+      classification = "StarWars";
+    } else if (!creature.isHumanoid() && "Endor".equalsIgnoreCase(creature.getUniverse())) {
+      starWars.individuals().add(mapper.valueToTree(creature));
+      classification = "StarWars";
+    } else if (creature.getTraits().contains("HAIRY") && creature.getTraits().contains("TALL") && creature.getAge() <= 400) {
+      starWars.individuals().add(mapper.valueToTree(creature));
+      classification = "StarWars";
+    } else if (creature.getTraits().contains("SHORT") && creature.getTraits().contains("HAIRY") && creature.getAge() <= 60) {
+      starWars.individuals().add(mapper.valueToTree(creature));
+      classification = "StarWars";
     }
 
-    System.out.println("\nCreatures with even IDs:");
-    for (int i = 0; i < data.size(); i++) {
-      if (i % 2 == 0) {
-        System.out.println(data.get(i).toPrettyString());
+    // Lord of the Rings classification
+    else if ("Earth".equalsIgnoreCase(creature.getUniverse())) {
+      if (creature.isHumanoid() && creature.getTraits().contains("BLONDE") && creature.getTraits().contains("POINTY_EARS")) {
+        rings.individuals().add(mapper.valueToTree(creature));
+        classification = "Lord of the Rings";
+      } else if (creature.isHumanoid() && creature.getTraits().contains("SHORT") && creature.getTraits().contains("BULKY")) {
+        rings.individuals().add(mapper.valueToTree(creature));
+        classification = "Lord of the Rings";
+      } else if (creature.isHumanoid() && creature.getAge() > 200) {
+        rings.individuals().add(mapper.valueToTree(creature));
+        classification = "Lord of the Rings";
+      } else if (creature.getTraits().contains("BLONDE") && creature.getTraits().contains("TALL")) {
+        rings.individuals().add(mapper.valueToTree(creature));
+        classification = "Lord of the Rings";
+      } else {
+        rings.individuals().add(mapper.valueToTree(creature));
+        classification = "Lord of the Rings";
       }
     }
 
-    System.out.println("\nCreatures with odd IDs:");
-    for (int i = 0; i < data.size(); i++) {
-      if (i % 2 != 0) {
-        System.out.println(data.get(i).toPrettyString());
-      }
+    // Marvel classification
+    else if (creature.isHumanoid() && "Asgard".equalsIgnoreCase(creature.getUniverse())) {
+      marvel.individuals().add(mapper.valueToTree(creature));
+      classification = "Marvel";
+    } else if (creature.getTraits().contains("BLONDE") && creature.getTraits().contains("TALL") && creature.getAge() <= 5000) {
+      marvel.individuals().add(mapper.valueToTree(creature));
+      classification = "Marvel";
     }
+
+    // Hitchhikers classification
+    else if (creature.isHumanoid() && "Betelgeuse".equalsIgnoreCase(creature.getUniverse())) {
+      hitchhikers.individuals().add(mapper.valueToTree(creature));
+      classification = "Hitchhikers";
+    } else if (creature.getTraits().contains("EXTRA_ARMS") || creature.getTraits().contains("EXTRA_HEAD")) {
+      hitchhikers.individuals().add(mapper.valueToTree(creature));
+      classification = "Hitchhikers";
+    } else if (!creature.isHumanoid() && (creature.getTraits().contains("GREEN") || creature.getTraits().contains("BULKY"))) {
+      hitchhikers.individuals().add(mapper.valueToTree(creature));
+      classification = "Hitchhikers";
+    }
+
+    // Additional conditions for Lord of the Rings
+    else if (creature.isHumanoid() && creature.getTraits().contains("BULKY")) {
+      rings.individuals().add(mapper.valueToTree(creature));
+      classification = "Lord of the Rings";
+    } else if (creature.isHumanoid() && creature.getAge() > 1000) {
+      rings.individuals().add(mapper.valueToTree(creature));
+      classification = "Lord of the Rings";
+    }
+
+    return classification;
   }
 }
 
